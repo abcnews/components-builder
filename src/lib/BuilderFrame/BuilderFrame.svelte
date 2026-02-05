749b738a5aece92e63d15485115c16538d501d74
Simon Elvery <simon@elvery.net>
2026-02-05 12:07:42 +1000

    Make it possible to resize the viz frame in the builder

--- src/lib/BuilderFrame/BuilderFrame.svelte
+++ src/lib/BuilderFrame/BuilderFrame.svelte
@@ -1,9 +1,82 @@
 <script lang="ts">
   let { Viz, Sidebar } = $props();
+
+  let vizAreaWidth = $state();
+  let vizAreaHeight = $state();
+
+  let vizDimensions: [number, number] = $state([Infinity, Infinity]);
+  let presetValue: [number, number] | "custom" | "auto" = $state("auto");
+
+  $effect(() => {
+    if (Array.isArray(presetValue)) {
+      vizDimensions = [presetValue[0], presetValue[1]];
+    }
+  });
+
+  $effect(() => {
+    const match = commonViewports
+      .values()
+      .find((d) => d[0] === vizDimensions[0] && d[1] === vizDimensions[1]);
+
+    if (!match) {
+      presetValue =
+        vizAreaWidth === vizDimensions[0] && vizAreaHeight === vizDimensions[1]
+          ? "auto"
+          : "custom";
+    }
+  });
+
+  const commonViewports = new Map<string, [number, number]>([
+    ["Desktop (large)", [1920, 1080]],
+    ["Desktop (small)", [1280, 720]],
+    ["iPad Pro", [1024, 1366]],
+    ["Surface Pro 7", [912, 1368]],
+    ["Gallaxy Note 20", [412, 915]],
+    ["iPhone 14 Pro Max", [430, 932]],
+    ["iPhone SE", [375, 667]],
+  ]);
 </script>
 
 <div class="builder-frame">
-  <div class="builder-frame__viz">{@render Viz?.()}</div>
+  <div class="builder-frame__viz">
+    <div class="tools">
+      <select name="resize-options" bind:value={presetValue}>
+        <option value={"auto"} selected>Auto</option>
+        <option value={"custom"}>Custom</option>
+        {#each commonViewports.entries() as [name, dimensions]}
+          <option value={dimensions}>{name}</option>
+        {/each}
+      </select>
+      <input
+        disabled={presetValue === "auto"}
+        id="viz-width"
+        type="number"
+        bind:value={vizDimensions[0]}
+      />
+      x
+      <input
+        disabled={presetValue === "auto"}
+        id="viz-height"
+        type="number"
+        bind:value={vizDimensions[1]}
+      />
+    </div>
+    <div
+      class="content-area"
+      bind:clientWidth={vizAreaWidth}
+      bind:clientHeight={vizAreaHeight}
+    >
+      <div
+        class="resize-frame"
+        bind:offsetWidth={vizDimensions[0]}
+        bind:offsetHeight={vizDimensions[1]}
+        style:width={presetValue === "auto" ? "100%" : `${vizDimensions[0]}px`}
+        style:height={presetValue === "auto" ? "100%" : `${vizDimensions[1]}px`}
+      >
+        {@render Viz?.()}
+      </div>
+    </div>
+  </div>
 
   <div class="builder-frame__sidebar">{@render Sidebar?.()}</div>
 </div>
@@ -20,16 +93,51 @@
     color: var(--text);
   }
   .builder-frame__viz {
+    display: grid;
+    grid-template-columns: 100%;
+    grid-template-rows: min-content 1fr;
     flex: 1;
     min-width: 50%;
-    justify-content: center;
-    align-items: center;
     position: relative;
     overflow: hidden;
-    padding: 1.69rem;
-    background: white;
+    background: var(--background-alt);
     color: black;
+
+    > * {
+      margin: 5px;
+    }
+
+    .tools {
+      width: 100%;
+      text-align: center;
+      font-size: 0.8em;
+      margin-bottom: 0;
+      select {
+        width: 8em;
+      }
+
+      input[type="number"] {
+        width: 5em;
+        &[disabled] {
+          opacity: 0.3;
+        }
+      }
+    }
+
+    .content-area {
+      overflow: auto;
+      display: block;
+
+      .resize-frame {
+        resize: both;
+        overflow: auto;
+        background-color: var(--background);
+        border: 1px solid hsl(from var(--border) h s l / 0.2);
+        box-shadow: hsl(from var(--border) h s l / 0.1) 0 0 5px;
+      }
+    }
   }
+
   .builder-frame__sidebar {
     width: 22rem;
     padding: 2rem 1rem;
