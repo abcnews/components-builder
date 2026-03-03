87f09248f86de21521d70598560258d04b545960
Ash Kyd <kyd.ashley@abc.net.au>
2026-03-03 14:09:53 +1000

    feat: update to use svelte-bootstrap-icons

--- src/lib/MarkerAdmin/MarkerAdmin.svelte
+++ src/lib/MarkerAdmin/MarkerAdmin.svelte
@@ -5,6 +5,13 @@
   } from "$lib/shared-defaults.js";
   import { onMount, untrack } from "svelte";
   import { slide } from "svelte/transition";
+  import {
+    Floppy,
+    Copy,
+    Clipboard,
+    Trash,
+    ArrowCounterclockwise,
+  } from "svelte-bootstrap-icons";
 
   let {
     /** Unique project name used in localStorage (e.g. "elections-federal2025-lower-house")*/
@@ -107,93 +114,23 @@
 </script>
 
 {#snippet saveIcon()}
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="16"
-    height="16"
-    fill="currentColor"
-    class="bi bi-floppy"
-    viewBox="0 0 16 16"
-  >
-    <title>Save</title>
-    <path d="M11 2H9v3h2z" />
-    <path
-      d="M1.5 0h11.586a1.5 1.5 0 0 1 1.06.44l1.415 1.414A1.5 1.5 0 0 1 16 2.914V14.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 14.5v-13A1.5 1.5 0 0 1 1.5 0M1 1.5v13a.5.5 0 0 0 .5.5H2v-4.5A1.5 1.5 0 0 1 3.5 9h9a1.5 1.5 0 0 1 1.5 1.5V15h.5a.5.5 0 0 0 .5-.5V2.914a.5.5 0 0 0-.146-.353l-1.415-1.415A.5.5 0 0 0 13.086 1H13v4.5A1.5 1.5 0 0 1 11.5 7h-7A1.5 1.5 0 0 1 3 5.5V1H1.5a.5.5 0 0 0-.5.5m3 4a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 .5-.5V1H4zM3 15h10v-4.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5z"
-    />
-  </svg>
+  <Floppy width="16" height="16" />
 {/snippet}
 
 {#snippet copyIcon()}
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="16"
-    height="16"
-    fill="currentColor"
-    class="bi bi-copy"
-    viewBox="0 0 16 16"
-  >
-    <title>Copy to clipboard</title>
-    <path
-      fill-rule="evenodd"
-      d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"
-    />
-  </svg>
+  <Copy width="16" height="16" />
 {/snippet}
 
 {#snippet pasteIcon()}
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="16"
-    height="16"
-    fill="currentColor"
-    class="bi bi-clipboard"
-    viewBox="0 0 16 16"
-  >
-    <title>Paste marker from clipboard</title>
-    <path
-      d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z"
-    />
-    <path
-      d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0z"
-    />
-  </svg>
+  <Clipboard width="16" height="16" />
 {/snippet}
 
 {#snippet deleteIcon()}
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="16"
-    height="16"
-    fill="currentColor"
-    class="bi bi-trash"
-    viewBox="0 0 16 16"
-  >
-    <path
-      d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"
-    />
-    <path
-      d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"
-    />
-  </svg>
+  <Trash width="16" height="16" />
 {/snippet}
 
 {#snippet undeleteIcon()}
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="16"
-    height="16"
-    fill="currentColor"
-    class="bi bi-arrow-counterclockwise"
-    viewBox="0 0 16 16"
-  >
-    <path
-      fill-rule="evenodd"
-      d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2z"
-    />
-    <path
-      d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466"
-    />
-  </svg>
+  <ArrowCounterclockwise width="16" height="16" />
 {/snippet}
 
 <div class="toolbar">
