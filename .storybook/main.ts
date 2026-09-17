f9c8e37bc7e22a6bae2b64b1ad0085748b8545f6
Simon Elvery <simon@elvery.net>
2026-09-17 12:56:17 +1000

    Remove .mdx extension from stories config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,14 +1,11 @@
-import type { StorybookConfig } from '@storybook/sveltekit';
+import type { StorybookConfig } from "@storybook/sveltekit";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|ts|svelte)"
-  ],
-  "addons": ["@storybook/addon-svelte-csf"],
-  "framework": {
-    "name": "@storybook/sveltekit",
-    "options": {}
-  }
+  stories: ["../src/**/*.stories.@(js|ts|svelte)"],
+  addons: ["@storybook/addon-svelte-csf"],
+  framework: {
+    name: "@storybook/sveltekit",
+    options: {},
+  },
 };
 export default config;
